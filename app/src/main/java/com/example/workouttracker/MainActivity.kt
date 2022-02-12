540bb81677e9d6658709740369cd75b38cf2608b
Marmalaade <79687328+Marmalaade@users.noreply.github.com>
2022-02-13 01:26:26 +0300

    feat: add background music

--- app/src/main/java/com/example/workouttracker/MainActivity.kt
+++ app/src/main/java/com/example/workouttracker/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.workouttracker
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPLayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         hideSystemUI()
+        mediaPLayer = MediaPlayer.create(this, R.raw.background_music)
+        mediaPLayer.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPLayer.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPLayer.seekTo(mediaPLayer.currentPosition);
+        mediaPLayer.start();
     }
 
     private fun hideSystemUI() {
