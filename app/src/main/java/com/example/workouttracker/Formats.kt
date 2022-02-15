ce555bed9fea0f6103e35a40b3ab8e6a8cae2018
Marmalaade <79687328+Marmalaade@users.noreply.github.com>
2022-02-15 03:37:34 +0300

    feat: own viewHolder logic & appearance

--- app/src/main/java/com/example/workouttracker/Formats.kt
+++ app/src/main/java/com/example/workouttracker/Formats.kt
@@ -10,6 +10,8 @@ import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.workouttracker.database.Train
 import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 fun convertNumericQualityToString(quality: Int, resources: Resources): String {
     var qualityString = resources.getString(R.string.four_ok)
@@ -25,6 +27,8 @@ fun convertNumericQualityToString(quality: Int, resources: Resources): String {
     return qualityString
 }
 
+private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
+private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
 
 @SuppressLint("SimpleDateFormat")
 fun convertLongToDateString(systemTime: Long): String {
@@ -32,6 +36,24 @@ fun convertLongToDateString(systemTime: Long): String {
         .format(systemTime).toString()
 }
 
+fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
+    val durationMilli = endTimeMilli - startTimeMilli
+    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
+    return when {
+        durationMilli < ONE_MINUTE_MILLIS -> {
+            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.seconds_length, seconds, weekdayString)
+        }
+        durationMilli < ONE_HOUR_MILLIS -> {
+            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.minutes_length, minutes, weekdayString)
+        }
+        else -> {
+            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.hours_length, hours, weekdayString)
+        }
+    }
+}
 
 fun formatTrains(nights: List<Train>, resources: Resources): Spanned {
     val sb = StringBuilder()
@@ -45,7 +67,6 @@ fun formatTrains(nights: List<Train>, resources: Resources): Spanned {
                 append(resources.getString(R.string.end_time))
                 append("\t${convertLongToDateString(it.endTime)}<br>")
                 append(resources.getString(R.string.quality))
-                append("\t${convertNumericQualityToString(it.trainingQuality, resources)}<br>")
                 append(resources.getString(R.string.hours_train))
                 // Hours
                 append("\t ${it.endTime.minus(it.startTime) / 1000 / 60 / 60}:")
@@ -62,4 +83,5 @@ fun formatTrains(nights: List<Train>, resources: Resources): Spanned {
         return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
-class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+
+class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
--- app/src/main/java/com/example/workouttracker/workouttracker/WorkoutTrackerAdapter.kt
+++ app/src/main/java/com/example/workouttracker/workouttracker/WorkoutTrackerAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.workouttracker.workouttracker
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.workouttracker.R
-import com.example.workouttracker.TextItemViewHolder
+import com.example.workouttracker.convertDurationToFormatted
 import com.example.workouttracker.database.Train
 
-class WorkoutTrackerAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+class WorkoutTrackerAdapter : RecyclerView.Adapter<WorkoutTrackerAdapter.ViewHolder>() {
 
     var data = listOf<Train>()
         set(value) {
@@ -16,16 +18,36 @@ class WorkoutTrackerAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false)
-        return TextItemViewHolder(view as TextView)
+        val view = layoutInflater.inflate(R.layout.list_item_trainings_time, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.trainingId.toString()
+        val res = holder.itemView.context.resources
+        if (item.startTime == item.endTime) {
+            "I'm training ...".also { holder.trainingTime.text = it }
+        } else holder.trainingTime.text = convertDurationToFormatted(item.startTime, item.endTime, res)
+
+        holder.qualityImage.setImageResource(
+            when (item.trainingQuality) {
+                1 -> R.drawable.ic_mood_1
+                2 -> R.drawable.ic_mood_2
+                3 -> R.drawable.ic_mood_3
+                4 -> R.drawable.ic_mood_4
+                5 -> R.drawable.ic_mood_5
+                6 -> R.drawable.ic_mood_6
+                else -> R.drawable.training_cat
+            }
+        )
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val trainingTime: TextView = itemView.findViewById(R.id.training_lenght)
+        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
     }
 }
